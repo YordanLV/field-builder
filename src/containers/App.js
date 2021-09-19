f98977b8688d5d4de9de77865c72f82fd9cf9421
YordanLV <yordan.lasonov@gmail.com>
2021-09-19 22:49:05 +0300

    Clean up + styled-components init

---
+++ src/containers/App.js
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Field Builder
+        </h1>
+      </header>
+    </div>
+  );
+}
