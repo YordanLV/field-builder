7002508db164d0fd24d40ec328b80b28a24c362c
YordanLV <yordan.lasonov@gmail.com>
2021-09-20 10:57:51 +0300

    Added axios and localstorage

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState, useCallback } from "react";
+import axios from "axios";
 
 import Input from "../components/Input";
 import Textarea from "../components/ArrayTextArea";
@@ -15,6 +16,22 @@ export default function App() {
 
   const [fields, setFields] = useState(defaultFields);
   const [error, setError] = useState("");
+
+  const saveLocalFields = () => {
+    localStorage.setItem("fields", JSON.stringify(fields));
+  };
+  const getSaveLocalFields = useCallback(() => {
+    if (localStorage.getItem("fields") === null) {
+      localStorage.setItem("fields", JSON.stringify(fields));
+    } else {
+      let fieldsLocal = JSON.parse(localStorage.getItem("fields"));
+      setFields(fieldsLocal);
+    }
+  }, [fields]);
+
+  useEffect(() => {
+    getSaveLocalFields();
+  }, [getSaveLocalFields]);
 
   const setLabel = (event) => {
     const value = event.target.value;
@@ -51,15 +68,30 @@ export default function App() {
   const validation = () => {
     if (fields.label === "") {
       setError("Label is required!");
+      return false;
     } else if (checkForDuplicates(fields.choices)) {
       setError("An option is duplicated!");
+      return false;
     } else if (fields.choices.length >= OPTIONS_LIMIT) {
       setError(`Options should not exceed more than ${OPTIONS_LIMIT}`);
+      return false;
     }
+    return true;
   };
-  const submit = () => {
-    validation();
+
+  const handleSubmit = () => {
     addDefaultValue();
+    if (validation()) {
+      axios
+        .post("http://www.mocky.io/v2/566061f21200008e3aabd919", {
+          ...fields,
+          choices: [...fields.choices, fields.defaultValue],
+        })
+        .then((res) => {
+          console.log(res);
+          console.log(res.data);
+        });
+    }
   };
 
   const stringChoices = fields.choices.join("\n");
@@ -78,11 +110,11 @@ export default function App() {
       />
       <Textarea label="Choices" onChange={setChoices} value={stringChoices} />
       <Button
-        onClick={arrangeAlphabetical}
         label="Display Choices Alphabetical"
+        onClick={arrangeAlphabetical}
       />
       <Button onClick={resetFields} label="Cancel" />
-      <Button onClick={submit} label="Submit" />
+      <Button onClick={handleSubmit} label="Submit" />
     </div>
   );
 }
